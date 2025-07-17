3ac4f2b12a6bc2f775080320d67d74cb5a24f7ff
ViniciusTn <vinicius_t_nunes@estudante.sesisenai.org.br>
2025-07-17 09:10:54 -0300

    exercicio8

--- exercicio8.php
+++ exercicio8.php
@@ -2,9 +2,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Número pares</title>
 </head>
 <body>
-    
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar">Verificar</button>
+   </form>
+
+   <?php 
+   if (isset($_POST['verificar'])) {
+        $numero = (int) $_POST['numero'];
+
+        
+        function contarPares($numero) {
+            return floor($numero / 2);
+        }
+
+
+        $pares = contarPares($numero);
+        echo "<hr>";
+        echo "<p><strong>Número informado:</strong> $numero</p>";
+        echo "<p>Existem <strong>$pares</strong> números pares entre 1 e $numero.</p>";
+   }
+   ?>
 </body>
 </html>
